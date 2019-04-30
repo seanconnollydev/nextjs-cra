a4ab581a566ab4479e1d35eb95ac785e6e1c3708
Sean Connolly <sconnolly17@gmail.com>
2019-04-29 20:49:51 -0600

    Serve locally

--- api/serve.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const data = require('./data');
-const app = express();
-const port = 3002;
-
-app.get('/api/data', data);
-
-app.listen(port, () => console.log(`Listening on port ${port}!`))
---
+++ serve.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const data = require('./api/data');
+const app = express();
+const port = 3002;
+
+app.get('/api/data', data);
+app.use('/nextjs', proxy({
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+}));
+app.use('/', proxy({
+  target: 'http://localhost:3000',
+  changeOrigin: true,
+}))
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
