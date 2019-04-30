ff5d57f5cb3335ace68753618c0ceec57a4a3bb9
Sean Connolly <sconnolly17@gmail.com>
2019-04-29 20:42:35 -0600

    Serve api locally

--- api/serve.js
+++ api/serve.js
@@ -5,4 +5,4 @@ const port = 3002;
 
 app.get('/api/data', data);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,3 +1,8 @@
 const Index = () => (<div>Next.js</div>);
 
+Index.getInitialProps = async({ req }) => {
+  console.log('req.headers.host', req.headers.host);
+  return {};
+}
+
 export default Index;
