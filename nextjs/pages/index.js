07f402b244aa2f630352c1cfb492a73a95ff4163
Sean Connolly <sconnolly17@gmail.com>
2019-04-29 21:54:12 -0600

    Hook up to api

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,7 +1,12 @@
+import axios from 'axios';
+
 const Index = () => (<div>Next.js</div>);
 
 Index.getInitialProps = async({ req }) => {
+  const host = req ? req.headers.host : '';
   console.log('req.headers.host', req.headers.host);
+  const resp = await axios.get(`http://${host}/api/data`);
+  console.log('resp', resp.data);
   return {};
 }
 
--- serve.js
+++ serve.js
@@ -7,12 +7,10 @@ const port = 3002;
 app.get('/api/data', data);
 app.use('/nextjs', proxy({
   target: 'http://localhost:3001',
-  changeOrigin: true,
   pathRewrite: { '^/nextjs': '' }, // e.g. /nextjs/index -> /index
 }));
 app.use('/', proxy({
   target: 'http://localhost:3000',
-  changeOrigin: true,
 }))
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
