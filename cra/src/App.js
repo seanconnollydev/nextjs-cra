93bd98453bc40eaa282ebf2679eb92881fdc6eb3
Sean Connolly <sconnolly17@gmail.com>
2019-05-04 19:41:44 -0600

    Fetch data with cra

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await axios.get('/api/data');
+      const users = resp.data.map(({ id, firstName }) => ({ id, firstName }));
+      setUsers(users);
+    };
+    fetchData();
+  }, []);
+
+  if (users.length === 0) return 'Loading...';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{users.map(u => (
+      <div key={u.id}>{u.firstName}</div>
+    ))}</div>
   );
 }
 
